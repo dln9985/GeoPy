2242a6813b732926c57a946f8ada0062b0d3dc36
Hassan-1-bite <laura.hassan@uteca.edu.mx>
2024-01-23 08:41:06 -0600

    Herencia de clases

--- lib/Poligon.py
+++ lib/Poligon.py
@@ -1,3 +1,4 @@
+# ------ Comienzo
 class Poligono:
     def __init__(self, Numlado, Sizelado):
         self.Numlado = Numlado
@@ -5,4 +6,40 @@ class Poligono:
         pass
     
     def __str__(self):
-        return f"Número de Lados:"
+        return f"Número de Lados: {self.Numlado} y Tamaño del lado: {self.Sizelado} "
+    
+    def Figure(self):
+        match self.Numlado:
+            case 3:
+                return f"Tu poligono es un triángulo dado a que tiene {self.Numlado} lados"
+            case 4:
+                return f"Tu poligono es un cuadrado dado a que tiene {self.Numlado} lados"
+            case 5:
+                return f"Tu poligono es un pentágono dado a que tiene {self.Numlado} lados"
+            case 6:
+                return f"Tu poligono es un hexágono dado a que tiene {self.Numlado} lados"
+            case 7:
+                return f"Tu poligono es un heptágono dado a que tiene {self.Numlado} lados"
+            case 8:
+                return f"Tu poligono es un octágono dado a que tiene {self.Numlado} lados"
+            case 9:
+                return f"Tu poligono es un eneágono dado a que tiene {self.Numlado} lados"
+            case _:
+                return "No reconozco tu poligono :("
+    
+    def Perimeter(self):
+        return f"El perimetro de tu poligono es de {self.Numlado * self.Sizelado} centimetros [cm]"
+            
+# ------ Final
+    
+class PoligonoRegular( Poligono ):
+    def __init__(self, Numlado, Sizelado, Apotema):
+        super().__init__(Numlado, Sizelado)
+        self.Apotema = Apotema
+        pass
+    
+    def __str__(self):
+        return f"Número de lados: {self.Numlado}, Tamaño de los lados: {self.Sizelado}, Apotema: {self.Apotema}"
+    
+    def AreaPoly(self):
+        return f"El área de tu poligono es de {self.Numlado * self.Sizelado * self.Apotema/2} centimetros cuadrados [cm^2]"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lib import *
 
-obj_rect = Rectangulo(10,5)
+"""obj_rect = Rectangulo(10,5)
 print(obj_rect)
 
 print("Ancho: "+str(obj_rect.ancho))
@@ -21,4 +21,24 @@ print("Ancho: "+str(obj_rect_3.ancho)+"[cm]")
 print("Largo: "+str(obj_rect_3.largo)+"[cm]")
 
 print("Perimetro: "+str(obj_rect_3.perimetro())+"[cm]")
-print("Área: "+str(obj_rect_3.area())+"[cm^2]")
+print("Área: "+str(obj_rect_3.area())+"[cm^2]")"""
+print("")
+
+obj_poly_1 = Poligono(5,18)
+print(obj_poly_1)
+
+print(f"Número de lados: {obj_poly_1.Numlado } ")
+print(f"Tamaño de los lados: {obj_poly_1.Sizelado } ")
+print(obj_poly_1.Figure() )
+
+print(obj_poly_1.Perimeter())
+
+print("")
+
+obj_poly_2 = PoligonoRegular(9,15,30)
+print(obj_poly_2)
+print(obj_poly_2.Figure())
+print(obj_poly_2.Perimeter() )
+print(obj_poly_2.AreaPoly() )
+
+print("")
